6e47b9d9737f2b39d5cae5d6dc8e6e79ea5a3eec
Romesh <romeshfernandodean98@gmail.com>
2022-11-29 15:57:11 +0530

    ew

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 use Session;
 
@@ -18,7 +19,16 @@ class Controller extends BaseController
     public function set_session(Request $request, $value)
     {
         $request->session()->put('ss', $value);
-        dd('Session ss is : '.session()->get('ss'));
+        //dd('Session ss is : '.session()->get('ss')."\n");
+        // dd('All Sessions : '.Redis::connection('session')->keys('*'));
+        $keys = array_map(fn($key) => str_replace('laravel_database_', '', $key), Redis::keys('*'));
+        $data = array_map(fn($data) => unserialize(unserialize($data)), Redis::mget($keys));
+        echo '<pre>';
+        var_dump('Session ss is : '.session()->get('ss')."\n");
+        var_dump('Session ss2 is : '.session()->get('ss2')."\n");
+        var_dump('Session ss3 is : '.session()->get('ss3')."\n");
+        var_dump($data);
+        echo '</pre>';
     }
 
     public function delete_session()
--- config/database.php
+++ config/database.php
@@ -121,7 +121,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
@@ -144,6 +144,15 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
+        ],
+
+        'session' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => '0',
+            'prefix' => 's:'
         ],
 
     ],
